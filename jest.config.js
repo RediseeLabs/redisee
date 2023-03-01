d501188cf1d1413fc803ea33a0d3ffd6fab49866
Anna Ivakhnik <annaivakhnik@Annas-MacBook-Pro.local>
2023-03-01 12:16:04 -0500

    save to overwrite conflict later

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["/jest.setup.js"],
+};

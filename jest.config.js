3980ce589e03f64bd6e3ab7d15eb2093d0673546
GarrettYan <garrettyan6@gmail.com>
2023-03-01 20:57:09 -0500

    Update jest.config and jest.setup

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,3 @@
-// module.exports = {
-//   preset: 'ts-jest',
-//   modulePaths: ['/shared/vendor/modules'],
-//   moduleFileExtensions: ['js', 'jsx'],
-//   moduleDirectories: ['node_modules', 'bower_components', 'shared'],
-
-//   moduleNameMapper: {
-//     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-//     '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
-
-//     // '^react(.*)$': '<rootDir>/vendor/react-master$1',
-//     '^config$': '<rootDir>/configs/app-config.js',
-//   },
-//   transform: {
-//     '^.+\\.(ts|tsx)?$': 'ts-jest',
-//     '^.+\\.(js|jsx)$': 'babel-jest',
-//   },
-//   transformIgnorePatterns: [
-//     '/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$',
-//     '/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$',
-//     '/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$',
-//   ],
-// };
-
-// module.exports = {
-//   collectCoverage: true,
-//   collectCoverageFrom: ['src/**/*.{js,jsx}'],
-//   coverageDirectory: 'coverage',
-//   testEnvironment: 'jsdom',
-//   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-// };
-
 module.exports = {
   collectCoverage: false,
   collectCoverageFrom: ['client/**/*.{js,jsx}'],
